5730c14e8a135c27e398568b2e7550d80a37868b
Farshad Nematdoust <farshad@nematdoust.com>
2020-10-13 23:41:42 +0200

    Update Code style.

--- main.go
+++ main.go
@@ -9,25 +9,25 @@ import (
 
 func main() {
 
-	user :=os.Getenv("DNS_USERNAME")
-	password :=os.Getenv("DNS_PASSWORD")
-	hostname :=os.Getenv("HOSTNAME")
+	user := os.Getenv("DNS_USERNAME")
+	password := os.Getenv("DNS_PASSWORD")
+	hostname := os.Getenv("HOSTNAME")
+
 	res, _ := http.Get("https://api.ipify.org")
 	ip, _ := ioutil.ReadAll(res.Body)
 
 	client := &http.Client{}
-	URL := "https://domains.google.com/nic/update?hostname="+hostname+"&"+string(ip)
+	URL := "https://domains.google.com/nic/update?hostname=" + hostname + "&" + string(ip)
 
 	fmt.Println(URL)
 
 	//pass the values to the request's body
-    	req, _ := http.NewRequest("POST", URL, nil)
-    	req.SetBasicAuth(user, password)
+	req, _ := http.NewRequest("POST", URL, nil)
+	req.SetBasicAuth(user, password)
 
-	resp, err:=client.Do(req)
+	resp, err := client.Do(req)
 
 	fmt.Println(resp)
 	fmt.Println(err)
 
 }
-
