866b2e39b518cc955c75c6106058135ae8720d6b
Farshad Nematdoust <farshad@nematdoust.com>
2020-10-13 23:40:24 +0200

    Sort the imports in the golang way.

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-		"net/http"
-		"io/ioutil"
-		"os"
-		"fmt"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
 func main() {
-	
+
 	user :=os.Getenv("DNS_USERNAME")
 	password :=os.Getenv("DNS_PASSWORD")
 	hostname :=os.Getenv("HOSTNAME")
@@ -17,17 +17,17 @@ func main() {
 
 	client := &http.Client{}
 	URL := "https://domains.google.com/nic/update?hostname="+hostname+"&"+string(ip)
-	
+
 	fmt.Println(URL)
-    	
+
 	//pass the values to the request's body
     	req, _ := http.NewRequest("POST", URL, nil)
     	req.SetBasicAuth(user, password)
-	
+
 	resp, err:=client.Do(req)
-	
+
 	fmt.Println(resp)
 	fmt.Println(err)
- 
+
 }
-		
+
